d972a4e74b934f422c93a0491cefba4fc696a679
hndko <hd.doko22@gmail.com>
2025-01-19 13:34:23 +0900

    Implement user registration with API authentication using Laravel Sanctum

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rules\Password;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        // Validasi input
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users,email',
+            'password' => ['required', 'confirmed', Password::default()]
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors(),
+                'message' => 'Registration failed. Please check your input.'
+            ], 422);
+        }
+
+        // Buat user baru
+        $user = new User();
+        $user::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        //Login user dan buat token
+        Auth::login($user);
+        $responseData = [
+            'token' => $user->createToken($user->name)->plainTextToken,
+            'name' => $user->name
+        ];
+
+        // Kirim response sukses
+        return response()->json([
+            'success' => true,
+            'data' => $responseData,
+            'message' => 'User successfully registered.'
+        ]);
+    }
+}
